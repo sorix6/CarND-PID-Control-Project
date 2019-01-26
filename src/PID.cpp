670eec7494ed388fcfe62882f2151c6f57a5a208
Sorix <elisa_s_d@yahoo.com>
2019-01-26 20:58:21 +0100

    First commit

---
+++ src/PID.cpp
@@ -0,0 +1,54 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+  this->Kp = Kp_;
+  this->Ki = Ki_;
+  this->Kd = Kd_;
+
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
+
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * TODO: Update PID errors based on cte.
+   */
+
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
+
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  // based on the formula presented in the Python code 
+  // steering = -tau_p * CTE - tau_d * diff_CTE - tau_i * int_CTE
+  double steering = -Kp * p_error - Ki * i_error - Kd * d_error;
+
+  // steering must be in the [-1, 1] interval
+  if (steering > 1){
+    return 1;
+  }
+  else if (steering < -1){
+    return -1;
+  }
+
+  return steering;
+  
+}
